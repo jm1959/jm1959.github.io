47a04e18b3749cd7caf4467dac72495b7f270977
jm1959 <112978130+jm1959@users.noreply.github.com>
2024-05-24 15:22:36 -0400

    color re-design

--- src/Footer.js
+++ src/Footer.js
@@ -4,12 +4,12 @@ import { MdEmail } from "react-icons/md";
 import { FaGithub } from "react-icons/fa";
 export default function Footer() {
     return (
-        <footer style={{ backgroundColor: "#946183", display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-            <p style={{ color: "#f4decb" }}>© 2024 by Joseph McGillen</p>
+        <footer style={{ backgroundColor: "#03045E", display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+            <p style={{ color: "#7272AB" }}>© 2024 by Joseph McGillen</p>
             <div>
-                <a href="mailto:mcgillenjoseph@gmail.com"><MdEmail style={{ fill: "#f4decb", margin: '0 10px' }} size={30} /></a>
-                <a href="https://www.linkedin.com/in/joseph-mcgillen/"><IoLogoLinkedin style={{ fill: "#f4decb", margin: '0 10px' }} size={30} /></a>
-                <a href="https://github.com/jm1959"><FaGithub style={{ fill: "#f4decb", margin: '0 10px' }} size={30} /></a>
+                <a href="mailto:mcgillenjoseph@gmail.com"><MdEmail style={{ fill: "#7272AB", margin: '0 10px' }} size={30} /></a>
+                <a href="https://www.linkedin.com/in/joseph-mcgillen/"><IoLogoLinkedin style={{ fill: "#7272AB", margin: '0 10px' }} size={30} /></a>
+                <a href="https://github.com/jm1959"><FaGithub style={{ fill: "#7272AB", margin: '0 10px' }} size={30} /></a>
             </div>
         </footer>
     );
--- src/components/buttons.js
+++ src/components/buttons.js
@@ -3,8 +3,8 @@ export const Buttons = ({ path }) => {
     return (
         <Link
             style={{
-                display: 'inline-block', padding: '10px 20px', color: '#f4decb',
-                backgroundColor: '#946183', borderRadius: '4px', textAlign: 'center', textDecoration: 'none'
+                display: 'inline-block', padding: '10px 20px', color: '#131200',
+                backgroundColor: '#7272AB', borderRadius: '4px', textAlign: 'center', textDecoration: 'none'
             }}
             to={path}>
             learn more
